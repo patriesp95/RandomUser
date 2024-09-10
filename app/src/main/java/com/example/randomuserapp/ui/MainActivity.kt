7a17839963c2370ae4cbcce5f6b37463d9dab8d5
Patricia Martinez Espert <patrimespert@outlook.com>
2024-09-10 18:06:53 +0200

    add menu and buttons

--- app/src/main/java/com/example/randomuserapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/randomuserapp/ui/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initListeners() {
-        binding.mainContainer.setOnClickListener {
+        binding.updateUser.setOnClickListener {
             randomUserViewModel.getRandomUser()
         }
     }
