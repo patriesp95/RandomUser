5dd6848e96d3e71e52f1e34c4b04a8b89aff6255
Patricia Martinez Espert <patrimespert@outlook.com>
2024-09-10 21:32:24 +0200

    minor changes

--- app/src/main/java/com/example/randomuserapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/randomuserapp/ui/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initListeners() {
-        binding.updateUser.setOnClickListener {
+        binding.mainContainer.setOnClickListener {
             randomUserViewModel.getRandomUser()
         }
     }
